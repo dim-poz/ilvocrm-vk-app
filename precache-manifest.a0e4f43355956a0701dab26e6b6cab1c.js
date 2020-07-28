867000428cd8a9b0566ade7d38efaab6e417cf35
Dimkys <x-18.ru@mail.ru>
2020-07-28 13:39:57 +0300

    Updates

---
+++ precache-manifest.a0e4f43355956a0701dab26e6b6cab1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11536ab3b3d175b44c5de2ce72c453a4",
+    "url": "/ilvocrm-vk-app/index.html"
+  },
+  {
+    "revision": "bde1e0fa6a220b9ab95c",
+    "url": "/ilvocrm-vk-app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "bde1e0fa6a220b9ab95c",
+    "url": "/ilvocrm-vk-app/static/js/2.ff1d9d89.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/ilvocrm-vk-app/static/js/2.ff1d9d89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3738f3e928cc066fc3b",
+    "url": "/ilvocrm-vk-app/static/js/main.239b44de.chunk.js"
+  },
+  {
+    "revision": "93fb09a01af8ecadbbd7",
+    "url": "/ilvocrm-vk-app/static/js/runtime-main.bab3bb1f.js"
+  }
+]);
